b36cf5c9b7689a8664aa53e001cd631efad8bed0
Omkar Nisal <omkar.nisal@gmailcom>
2013-02-18 15:47:10 +0530

    Initial Commit

---
+++ HelloOpenGL/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  HelloOpenGL
+//
+//  Created by Omkar on 18/02/13.
+//  Copyright (c) 2013 Omkar Nisal. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ HelloOpenGL/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  HelloOpenGL
+//
+//  Created by Omkar on 18/02/13.
+//  Copyright (c) 2013 Omkar Nisal. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
